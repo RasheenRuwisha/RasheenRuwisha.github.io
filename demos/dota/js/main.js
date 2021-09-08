68be7b788f3af86df53e20d25fc86470a58d1cbf
Rasheen <sheen.ruwisha@gmail.com>
2021-09-08 13:09:46 -0400

    Added j

---
+++ demos/dota/js/main.js
@@ -0,0 +1,131 @@
+$('.consumable-slick-slide-for').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.consumable-slick-slide-nav'
+});
+$('.consumable-slick-slide-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.consumable-slick-slide-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+});
+
+
+$('.assistant-slick-slide-for').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.assistant-slick-slide-nav'
+});
+$('.assistant-slick-slide-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.assistant-slick-slide-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
+
+$('.immortal-slick-for').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.immortal-slick-nav'
+});
+$('.immortal-slick-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.immortal-slick-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
+
+
+$('.immortal-1-item-slick').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.immortal-1-item-slick-nav'
+});
+$('.immortal-1-item-slick-nav').slick({
+    slidesToShow: 12,
+    slidesToScroll: 1,
+    asNavFor: '.immortal-1-item-slick',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
+
+
+$('.immortal-2-item-slick').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.immortal-2-item-slick-nav'
+});
+$('.immortal-2-item-slick-nav').slick({
+    slidesToShow: 12,
+    slidesToScroll: 1,
+    asNavFor: '.immortal-2-item-slick',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
+
+
+$('.immortal-3-item-slick').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.immortal-3-item-slick-nav'
+});
+$('.immortal-3-item-slick-nav').slick({
+    slidesToShow: 11,
+    slidesToScroll: 1,
+    asNavFor: '.immortal-3-item-slick',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
+
+
+
+$('.effect-slick-slide-for').slick({
+    swipe:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    // fade: true,
+    asNavFor: '.effect-slick-slide-nav'
+});
+$('.effect-slick-slide-nav').slick({
+    slidesToShow: 19,
+    slidesToScroll: 1,
+    asNavFor: '.effect-slick-slide-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
